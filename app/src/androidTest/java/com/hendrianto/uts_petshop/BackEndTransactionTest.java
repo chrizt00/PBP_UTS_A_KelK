2b2a65790b4e40c2db29abcad1d2ad7579165807
LENOVO <180709641@students.uajy.co.id>
2020-12-07 01:01:51 +0700

    UAS - Oracle Pet Shop

---
+++ app/src/androidTest/java/com/hendrianto/uts_petshop/BackEndTransactionTest.java
@@ -0,0 +1,211 @@
+package com.hendrianto.uts_petshop;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.hendrianto.uts_petshop.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BackEndTransactionTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void backEndTransactionTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.emailLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("admin"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.passwordLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("admin"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.passwordLogin), withText("admin"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText3.perform(pressImeActionButton());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction cardView = onView(
+                allOf(withId(R.id.btnTransaction),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        cardView.perform(scrollTo(), click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.btnSimpan), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction checkableImageButton = onView(
+                allOf(withId(R.id.text_input_end_icon), withContentDescription("Show dropdown menu"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.FrameLayout")),
+                                        2),
+                                0),
+                        isDisplayed()));
+        checkableImageButton.perform(click());
+
+        DataInteraction materialTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.btnSimpan), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.txtJumlah), withText("0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("5"));
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(R.id.txtJumlah), withText("5"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText2.perform(closeSoftKeyboard());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(withId(R.id.txtJumlah), withText("5"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText3.perform(pressImeActionButton());
+
+        ViewInteraction textInputEditText4 = onView(
+                allOf(withId(R.id.txtEmail), withText("Admin"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText4.perform(pressImeActionButton());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.btnSimpan), withText("Submit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.btnBack), withText("Back To Admin"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                0),
+                        isDisplayed()));
+        materialButton5.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ app/src/androidTest/java/com/hendrianto/uts_petshop/LoginTest.java
@@ -0,0 +1,290 @@
+package com.hendrianto.uts_petshop;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void loginTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.emailLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("abc"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.passwordLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText2.perform(pressImeActionButton());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.emailLogin), withText("abc"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("abc@gmail.com"));
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.emailLogin), withText("abc@gmail.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText4.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.passwordLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("123"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText6.perform(pressImeActionButton());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText7.perform(click());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText8.perform(click());
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText9.perform(replaceText("123456"));
+
+        ViewInteraction appCompatEditText10 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123456"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText10.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText11 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123456"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText11.perform(pressImeActionButton());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction appCompatEditText12 = onView(
+                allOf(withId(R.id.emailLogin), withText("abc@gmail.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText12.perform(replaceText("hendrianto8901234@gmail.com"));
+
+        ViewInteraction appCompatEditText13 = onView(
+                allOf(withId(R.id.emailLogin), withText("hendrianto8901234@gmail.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText13.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText14 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123456"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText14.perform(pressImeActionButton());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction appCompatEditText15 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123456"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText15.perform(click());
+
+        ViewInteraction appCompatEditText16 = onView(
+                allOf(withId(R.id.passwordLogin), withText("123456"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText16.perform(replaceText("abcdefg"));
+
+        ViewInteraction appCompatEditText17 = onView(
+                allOf(withId(R.id.passwordLogin), withText("abcdefg"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText17.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText18 = onView(
+                allOf(withId(R.id.passwordLogin), withText("abcdefg"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText18.perform(pressImeActionButton());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.btnLogin), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.core.widget.NestedScrollView")),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton5.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
